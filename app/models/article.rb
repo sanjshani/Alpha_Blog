280f9e442925f7b1946962b5495c7e5751c2bbf8
sanjshani <sanjshani@yahoo.com>
2019-10-04 21:19:15 -0400

    create template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
-  # Validation can be of either of the 2 ways described below!!
   validates :title, presence: true, length: {minimum: 5, maximum: 50}
-  validates_presence_of :description, length: {minimum: 10, maximum: 150}
+  validates :description, presence: true, length: {minimum: 10, maximum: 150}
 end
