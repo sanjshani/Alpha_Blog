fd8abdbf81fa2bc60b6c44b9279bd0fc6e60ae42
sanjshani <sanjshani@yahoo.com>
2019-10-28 13:09:14 -0400

    add automatic login with sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the AlphaBlog #{@user.username.capitalize}"
-      redirect_to articles_path(@user)
+      redirect_to user_path(@user)
     else
       render 'new'
     end
